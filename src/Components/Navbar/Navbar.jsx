d05e263decf2ba62671f84cd52d269c1811f4a5c
Bee Acree <tacree@charlotte.edu>
2026-04-22 12:03:54 -0400

    Feat: added Navbar component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    
+
+    return(
+        <nav>
+            <ul className='text-white flex flex-row justify-center items-center gap-24 list-none p-5'>
+                <li>Placeholder</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar
